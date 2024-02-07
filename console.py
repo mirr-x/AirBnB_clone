36528da12d66383224d4f321f4e150dc371c169f
amir <amir.exe@icloud.com>
2024-02-07 01:18:51 +0100

    kl

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -33,13 +34,13 @@ class HBNBCommand(cmd.Cmd):
             instance_id = instance.id
             print(instance_id)
 
-    def do_show(self,content):
+    def do_show(self, content):
 
         ln = len(content)
         lis = content.split(" ")
         lis_ln = len(lis)
 
-        if ln == 0 :
+        if ln == 0:
             print("** class name missing **")
         elif lis[0] not in globals():
             print("** class doesn't exist **")
@@ -47,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             json_dict = storage.all()
-            key = "{:s}.{:s}".format(lis[0],lis[1])
+            key = "{:s}.{:s}".format(lis[0], lis[1])
 
             x = False
             for i in json_dict.keys():
@@ -55,21 +56,50 @@ class HBNBCommand(cmd.Cmd):
                     x = True
                     break
 
-            if x == True :
-                value =  json_dict[key]
-                full_format = "[{:s}] ({:s}) {:s}".format(lis[0],lis[1],str(value))
+            if x == True:
+                value = json_dict[key]
+                full_format = "[{:s}] ({:s}) {:s}".format(lis[0], lis[1], str(value))
                 print(full_format)
             else:
                 print("** no instance found **")
 
-    #todo
-    def do_destroy(self,content):
-        
+    # todo
+    def do_destroy(self, content):
+
+        lis = content.split(" ")
+        ln = len(content)
+        lis_ln = len(lis)
+        bol = None
+
+        if ln == 0:
+            print("class name missing")
+        elif lis[0] not in globals():
+            print("** class doesn't exist **")
+        elif lis_ln == 1:
+            print("** instance id missing **")
+        else:
+            json_dict = storage.all()
+            key_form = "{:s}.{:s}".format(lis[0], lis[1])
+
+            for i in json_dict.keys():
+                if key_form == i: 
+                    del json_dict[key_form]
+                    
+                    with open("file.json", "w") as f:
+                        json.dump(json_dict, f, indent=2)
+
+                    bol = True
+                    break
+            if bol != True:
+                print("** no instance found **")
+
+
+
+        print("z")
 
         pass
 
     #!!!!!!!!!!!!!!!
-    
 
 
 if __name__ == '__main__':
