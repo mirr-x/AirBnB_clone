83d30f2d5eee036cf8c6cec7cdc1c6cd191f6ca7
amir <amir.exe@icloud.com>
2024-02-08 01:27:47 +0100

    bnb

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,10 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         self.my_number = None
         self.name = None
-        self.updated_at = datetime.now()
         self.id = str(uuid4())
         self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
 
         # storage
         from models import storage
