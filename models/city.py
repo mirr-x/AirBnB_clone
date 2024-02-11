ce51f0dc0392fc407ade6bca4ab76d46b913e913
amir <amir.exe@icloud.com>
2024-02-11 12:02:25 +0100

    bnb

--- models/city.py
+++ models/city.py
@@ -3,6 +3,11 @@
 from models.base_model import BaseModel
 
 class City(BaseModel):
+    '''
+    @state_id: string - empty string: it will be the State.id
+    @name: string - empty string
 
-    state_id = ""
+    '''
+
+    state_id = "" 
     name = ""
